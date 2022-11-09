2b660da7c58c9948572557e9c0ee5cc4240b7f38
MoinulIslam7 <moinul180180@gmail.com>
2022-11-09 14:48:25 +0600

    added somthing

--- index.js
+++ index.js
@@ -12,8 +12,20 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0tydy0p.mongodb.net/?retryWrites=true&w=majority`;
 
+console.log(uri)
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+async function run() {
+    try {
+        const serviceCollection = client.db('serviceReview').collection('services');
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error));
 
 
 
