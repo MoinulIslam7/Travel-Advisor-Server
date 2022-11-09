4f482f337ce86b5e43a39ee34c0646501b0f55a0
MoinulIslam7 <moinul180180@gmail.com>
2022-11-09 14:06:08 +0600

    added database

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0tydy0p.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 
 app.get('/', (req, res) => {
