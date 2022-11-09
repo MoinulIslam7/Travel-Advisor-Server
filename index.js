4c1297d3428c75854918c1a8a30080b10605ce32
MoinulIslam7 <moinul180180@gmail.com>
2022-11-10 04:14:32 +0600

    delete review added

--- index.js
+++ index.js
@@ -74,6 +74,12 @@ async function run() {
             const reviews = await cursor.toArray();
             res.send(reviews);
         })
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id)};
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        });
     }
     finally {
 
